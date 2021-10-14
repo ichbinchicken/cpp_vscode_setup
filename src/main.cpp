fee7553b010ff6db5bb0eafd7fa8b9cc0a8f80bb
Toby Zheng <tobbyzheng0807@gmail.com>
2021-10-14 11:43:53 +1100

    update 1

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,14 @@
 #include <range/v3/all.hpp>
 #include <string>
 #include <iostream>
+#include <absl/container/flat_hash_set.h>
 
 
 auto main() -> int {
   fmt::print("Hello, world!\n");
-  std::string s{"hello world"};
+  std::string s{"hello d"};
     // output: h e l l o
     ranges::for_each(s, [](char c) { std::cout << c << ' '; });
     std::cout << '\n';
+
 }
